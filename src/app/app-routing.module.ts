1a4f02d8765da6aa577cfea4c604b7dae400ce27
melvin <melvinjoseph303@gmail.com>
2021-07-16 18:08:29 +0530

    first commit : first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PartnershipComponent } from './partnership/partnership.component';
+import { PartnershipformComponent } from './partnershipform/partnershipform.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'partnership',
+    component: PartnershipComponent
+  },
+  {
+    path: 'partnershipform',
+    component: PartnershipformComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PartnershipComponent } from './partnership/partnership.component';
+import { PartnershipformComponent } from './partnershipform/partnershipform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PartnershipComponent,
+    PartnershipformComponent
   ],
   imports: [
     BrowserModule,
