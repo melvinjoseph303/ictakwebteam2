f659f8c61d89ebdb69a5cdc02fc31a15c4ea645b
melvin <melvinjoseph303@gmail.com>
2021-07-23 09:30:32 +0530

    first commit : first version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PartnershipComponent } from './partnership/partnership.component';
 import { PartnershipformComponent } from './partnershipform/partnershipform.component';
+import { AcademicmembershipComponent } from './academicmembership/academicmembership.component';
+import { CorporatemembershipComponent } from './corporatemembership/corporatemembership.component';
+import { CorporatemembershipcontactusformComponent } from './corporatemembershipcontactusform/corporatemembershipcontactusform.component';
+import { CorporatemembershipregisternowformComponent } from './corporatemembershipregisternowform/corporatemembershipregisternowform.component';
+import { ViewacademicmembersComponent } from './viewacademicmembers/viewacademicmembers.component';
 const routes: Routes = [
   {
     path: '',
@@ -15,6 +20,26 @@ const routes: Routes = [
   {
     path: 'partnershipform',
     component: PartnershipformComponent
+  },
+  {
+    path: 'academicmembership',
+    component: AcademicmembershipComponent
+  },
+  {
+    path: 'corporatemembership',
+    component: CorporatemembershipComponent
+  },
+  {
+    path: 'contactus',
+    component: CorporatemembershipcontactusformComponent
+  },
+  {
+    path: 'registernow',
+    component: CorporatemembershipregisternowformComponent
+  },
+  {
+    path: 'viewacademicmembers',
+    component: ViewacademicmembersComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PartnershipComponent } from './partnership/partnership.component';
 import { PartnershipformComponent } from './partnershipform/partnershipform.component';
-
+import { AcademicmembershipComponent } from './academicmembership/academicmembership.component';
+import { CorporatemembershipComponent } from './corporatemembership/corporatemembership.component';
+import { CorporatemembershipcontactusformComponent } from './corporatemembershipcontactusform/corporatemembershipcontactusform.component';
+import { CorporatemembershipregisternowformComponent } from './corporatemembershipregisternowform/corporatemembershipregisternowform.component';
+import {MembershipService} from './membershipservice.service';
+import { ViewacademicmembersComponent } from './viewacademicmembers/viewacademicmembers.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PartnershipComponent,
-    PartnershipformComponent
+    PartnershipformComponent,
+    AcademicmembershipComponent,
+    CorporatemembershipComponent,
+    CorporatemembershipcontactusformComponent,
+    CorporatemembershipregisternowformComponent,
+    ViewacademicmembersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MembershipService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
